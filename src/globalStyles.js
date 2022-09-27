958d9ab4f0b6ee4119407a93b34b4cf9ad2d80a4
julia <agramartinsj@gmail.com>
2022-09-27 20:23:41 -0300

    added global style @styled-components

---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    body{
+        font-family: 'Montserrat', sans-serif;
+        background-color: #0F0E18;
+        color: #FAFAFA;
+    }
+
+`
