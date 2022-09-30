a0f5c5a94aa1093b947afaeb1bd8cab823587a1a
julia <agramartinsj@gmail.com>
2022-09-29 22:06:02 -0300

    added FA icon, onDelete and toggleDone functions

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import * as C from './styled.js'
+import { FaTimes } from 'react-icons/fa'
 
-function ListItem({ item }) {
+function ListItem({ item, onDelete, onToggle }) {
 	return (
-		<C.Container>
-			<li>
-				#{item.id} - {item.name}
-			</li>
-		</C.Container>
+		<C.Item done={item.done}>
+			<input type='checkbox' onClick={() => onToggle(item.id)} />
+			<label>
+				{item.name} - {`${item.done}`}
+			</label>
+			<FaTimes onClick={() => onDelete(item.id)} />
+		</C.Item>
 	)
 }
 
