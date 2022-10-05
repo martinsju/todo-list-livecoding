07f691e39bb1349ade14e87614511baeea57ef5e
julia <agramartinsj@gmail.com>
2022-10-04 21:43:24 -0300

    new components AddArea and Button

--- src/components/ListPage/styled.js
+++ src/components/ListPage/styled.js
@@ -3,12 +3,16 @@ import styled from 'styled-components'
 export const Container = styled.div`
 	margin: 0px 25px 0px 25px;
 `
+export const AddArea = styled.div`
+	display: flex;
+	margin-bottom: 10px;
+`
 export const Input = styled.input`
 	background: transparent;
 	border: 1px solid #373546;
 	border-radius: 5px;
 	padding: 10px;
-	margin-bottom: 5px;
+	margin-right: 10px;
 	width: 100%;
 	box-sizing: border-box;
 	color: #f7f7f7;
@@ -19,4 +23,11 @@ export const Input = styled.input`
 	:focus {
 		border: 1px solid #cecece;
 	}
+`
+export const Button = styled.button`
+	width: 20%;
+	border: 1px solid #373546;
+	border-radius: 5px;
+	font-family: 'Montserrat', sans-serif;
+	font-size: 15px;
 `
