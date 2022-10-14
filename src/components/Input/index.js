940d405cd8f5dec26eb90ef5832259e4270f9601
julia <agramartinsj@gmail.com>
2022-10-13 22:04:44 -0300

    refactor: created Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as C from './styled.js'
+
+function Input(props) {
+	return <C.Input {...props} />
+}
+
+export default Input
---
+++ src/components/Input/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+	width: 100%;
+	padding: 15px 20px;
+	background-color: #2d2d32;
+	color: #f7f7f7;
+	font-size: 16px;
+	border: none;
+	outline: none;
+	border-radius: 5px;
+	box-sizing: border-box;
+
+	::placeholder {
+		color: #a0a0a0;
+	}
+
+	:focus {
+		box-shadow: inset 0px 0px 0px 2px #bd93f9;
+	}
+`
