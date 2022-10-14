02a3171cf37ae270cd087e84146c668b73c98688
julia <agramartinsj@gmail.com>
2022-10-13 22:13:08 -0300

    style: changed bg color

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -2,9 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
     body{
-        font-family: 'Montserrat', sans-serif;
-        background-color: #201F2D;
-        color: #FAFAFA;
+        font-family: 'Poppins', sans-serif;
+        background-color: #1D1D20;
+        color: #F7F7F7;
+        margin: 0;
+        padding: 0;
     }
 
 `
