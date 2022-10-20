3c8f7a2f3f319c45bd7a6d8144b9fe22f17b5675
julia <agramartinsj@gmail.com>
2022-10-19 22:11:55 -0300

    style: added conditional bg color to button

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
+	background-color: ${({ color }) => color ?? '#f7f7f7'};
 	font-family: 'Poppins', sans-serif;
 	font-size: 16px;
 	color: #000;
 	white-space: nowrap;
+	border: none;
 	border-radius: 5px;
 	padding: 15px 20px;
 
 	:hover {
-		background-color: #bd93f9;
+		background-color: ${({ hoverColor }) => hoverColor ?? '#bd93f9'};
 	}
 `
