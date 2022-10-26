04f54791f158edf7b6eab8c05552bcf58526b704
julia <agramartinsj@gmail.com>
2022-10-25 21:41:32 -0300

    style: toast styling

--- src/components/ListPage/styled.js
+++ src/components/ListPage/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { ToastContainer } from 'react-toastify'
 
 export const Container = styled.div`
 	width: 800px;
@@ -18,4 +19,27 @@ export const Label = styled.div`
 	font-family: 'Montserrat', sans-serif;
 	font-size: 16px;
 	font-weight: 100;
+`
+
+export const StyledToast = styled(ToastContainer)`
+	.Toastify__toast-icon {
+		display: none;
+	}
+	.Toastify__toast-theme--colored {
+		color: #000;
+	}
+	.Toastify__toast-theme--colored.Toastify__toast--success {
+		background-color: #bcf39a;
+		border-left: 20px solid #8bc766;
+	}
+	.Toastify__toast-theme--colored.Toastify__toast--error {
+		background-color: #f47d7d;
+		border-left: 20px solid #e05a5a;
+	}
+	.Toastify__progress-bar-theme--colored.Toastify__progress-bar--success {
+		background-color: #8bc766;
+	}
+	.Toastify__progress-bar-theme--colored.Toastify__progress-bar--error {
+		background-color: #e05a5a;
+	}
 `
