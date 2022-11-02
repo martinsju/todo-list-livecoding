9d3b90ea326ad0568bd2a4e00073e18ba92aa078
julia <agramartinsj@gmail.com>
2022-11-02 15:24:00 -0300

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Header from './components/Header'
 import ListPage from './components/ListPage'
 import LoginPage from './components/LoginPage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<LoginPage />
-			{/* <ListPage /> */}
+			<Routes>
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='list' element={<ListPage />} />
+			</Routes>
 		</div>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import GlobalStyles from './globalStyles.js'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<GlobalStyles />
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )
 
