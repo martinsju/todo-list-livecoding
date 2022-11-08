bf841a3f5f2abe6a5a77d95fca5e43604355fb3f
julia <agramartinsj@gmail.com>
2022-11-08 18:00:24 -0300

    feat: added context and functions to LoginPage

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,23 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/Auth/AuthContext.js'
 import Button from '../Button/index.js'
 import Input from '../Input/index.js'
 import * as C from './styled.js'
 
 function LoginPage() {
+	const auth = useContext(AuthContext)
+
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const navigate = useNavigate()
+
+	async function handleLogin() {
+		console.log('email: ', email)
+		console.log('pw: ', password)
+		if (email && password) {
+			const isLogged = await auth.signin(email, password) //true || false
+			console.log('logged: ', isLogged)
+			if (isLogged) {
+				navigate('/list')
+			}
+		}
+	}
+
+	function handleInputEmail(e) {
+		setEmail(e.target.value)
+	}
+
+	function handleInputPassword(e) {
+		setPassword(e.target.value)
+	}
+
 	return (
 		<C.Container>
 			<C.Title>Faça o login</C.Title>
 			<C.InputContainer>
 				<C.InputArea>
 					<C.Label>Seu e-mail</C.Label>
-					<Input placeholder='Digite seu melhor e-mail' />
+					<Input
+						placeholder='Digite seu melhor e-mail'
+						type='text'
+						onChange={handleInputEmail}
+					/>
 				</C.InputArea>
 				<C.InputArea>
 					<C.Label>Sua senha</C.Label>
-					<Input placeholder='Digite sua senha' type='password' />
+					<Input
+						placeholder='Digite sua senha'
+						type='password'
+						onChange={handleInputPassword}
+					/>
 				</C.InputArea>
 			</C.InputContainer>
-			<Button>Login</Button>
+			<Button onClick={handleLogin}>Login</Button>
 		</C.Container>
 	)
 }
