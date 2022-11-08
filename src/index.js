6305851af24a5feef3d0041e53a8ca9809e352c3
julia <agramartinsj@gmail.com>
2022-11-08 18:00:41 -0300

    feat: added provider to all the application

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import GlobalStyles from './globalStyles.js'
 import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from './contexts/Auth/AuthProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<GlobalStyles />
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<AuthProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</AuthProvider>
 	</React.StrictMode>
 )
 
