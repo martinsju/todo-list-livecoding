accda134eab529bfd01ba5829f5206edac9a4e8c
julia <agramartinsj@gmail.com>
2022-11-08 17:59:07 -0300

    feat: create RequireAuth component

---
+++ src/contexts/Auth/RequireAuth.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { AuthContext } from './AuthContext'
+
+export function RequireAuth({ children }) {
+	const auth = useContext(AuthContext)
+	if (!auth.user) {
+		console.log('user: ', auth.user)
+		console.log("You're not logged in")
+		// return <LoginPage />
+		return <Navigate to='/' />
+	}
+	return children
+}
