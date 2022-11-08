b6da900dbe7fd5d9cc5c8d03bee47cc80b36cf29
julia <agramartinsj@gmail.com>
2022-11-08 18:01:23 -0300

    feat: added RequireAuth before go to private page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import Header from './components/Header'
 import ListPage from './components/ListPage'
 import LoginPage from './components/LoginPage'
 import { Routes, Route } from 'react-router-dom'
+import { RequireAuth } from './contexts/Auth/RequireAuth'
 
 function App() {
 	return (
 		<div>
 			<Header />
 			<Routes>
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='list' element={<ListPage />} />
+				<Route path='/' element={<LoginPage />} />
+				<Route
+					path='/list'
+					element={
+						<RequireAuth>
+							<ListPage />
+						</RequireAuth>
+					}
+				/>
 			</Routes>
 		</div>
 	)
