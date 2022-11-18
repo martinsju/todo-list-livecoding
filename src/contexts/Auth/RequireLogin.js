5105a01fe5005079c3a7ed9bbae337c99643db58
julia <agramartinsj@gmail.com>
2022-11-17 21:56:21 -0300

    feat: create require login component

---
+++ src/contexts/Auth/RequireLogin.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { AuthContext } from './AuthContext'
+
+export function RequireLogin({ children }) {
+	const auth = useContext(AuthContext)
+	if (auth.user) {
+		console.log('user: ', auth.user)
+		console.log("You're already logged in")
+		// return <ListPage />
+		return <Navigate to='/list' />
+	}
+	return children
+}
