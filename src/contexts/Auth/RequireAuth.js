d84746efe9951b7d9b77d0c974afc4a816153752
julia <agramartinsj@gmail.com>
2022-11-17 22:25:10 -0300

    chore: changed route

--- src/contexts/Auth/RequireAuth.js
+++ src/contexts/Auth/RequireAuth.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { Navigate } from 'react-router-dom'
 import { AuthContext } from './AuthContext'
+import { routes } from '../../config/routes'
 
 export function RequireAuth({ children }) {
 	const auth = useContext(AuthContext)
@@ -8,7 +9,7 @@ export function RequireAuth({ children }) {
 		console.log('user: ', auth.user)
 		console.log("You're not logged in")
 		// return <LoginPage />
-		return <Navigate to='/' />
+		return <Navigate to={routes.login.path} />
 	}
 	return children
 }
