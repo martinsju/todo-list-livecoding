97ac5a42a09b8232d4112b14dd68cf8c3e5244b8
julia <agramartinsj@gmail.com>
2022-12-13 20:28:23 -0300

    feat: created useModal hook for Modal control

---
+++ src/hooks/useModal.js
@@ -0,0 +1,23 @@
+import { useEffect, useCallback } from 'react'
+
+const KEY_NAME_ESC = 'Escape'
+const KEY_EVENT_TYPE = 'keyup'
+
+export function useModal(handleClose) {
+	const handleEscKey = useCallback(
+		(event) => {
+			if (event.key === KEY_NAME_ESC) {
+				handleClose()
+			}
+		},
+		[handleClose]
+	)
+
+	useEffect(() => {
+		document.addEventListener(KEY_EVENT_TYPE, handleEscKey, false)
+
+		return () => {
+			document.removeEventListener(KEY_EVENT_TYPE, handleEscKey, false)
+		}
+	}, [handleEscKey])
+}
