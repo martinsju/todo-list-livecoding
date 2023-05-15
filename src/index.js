8e256d90be3bf58c969b6b3561a41e4ed8c5c512
julia <agramartinsj@gmail.com>
2023-05-15 19:33:15 -0300

    chore: remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import GlobalStyles from './globalStyles.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
+	<>
 		<GlobalStyles />
 		<AuthProvider>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</AuthProvider>
-	</React.StrictMode>
+	</>
 )
